976c24ef4be23e22665c6321803a9b20d6e6323a
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-09 23:38:43 +0330

    update application structure

--- app/main.py
+++ app/main.py
@@ -1,49 +1,34 @@
 import asyncio
-from os import environ
-from time import time
 
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
-from requests import request
-
-STOCK_URL = environ.get("BTCUP_STOCK_URL")
-API_TOKEN = environ.get("BTCUP_API_TOKEN")
-
-INFLUX_URL = environ.get("BTCUP_INFLUX_URL", "http://influxdb:8086")
-INFLUX_SECRET = environ.get("BTCUP_INFLUX_SECRET", "secret")
-INFLUX_ORG = environ.get("BTCUP_INFLUX_ORG", "admin")
-
-
-def get_data() -> float:
-    query = f"{STOCK_URL}?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={API_TOKEN}"
-    response = request("GET", query)
-    data = response.json()
-    rate = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-    return rate
-
-
-def as_influxdb(currncy_rate: float, measurement_name: str = "currency_data") -> str:
-    currncy_rate = str(float)
-    timestamp = str(int(time.time() * 1e9))
-    return f"{measurement_name},btc rate={currncy_rate} {timestamp}"
-
-
-async def write_data(currency_data: str):
-    try:
-        async with InfluxDBClientAsync(
-            INFLUX_URL, token=INFLUX_SECRET, org=INFLUX_ORG
-        ) as c:
-            print(str(currency_data))
-            await c.write_api().write("currency", record=as_influxdb())
-    except Exception as e:
-        print(f"Error writing data: {e}")
+from .config import Config
+from .repositories import CurrencyRepository
+from .services import CurrencyService
+from .utils import InfluxDBWriter
 
 
 async def main():
+    config = Config()
+
+    api_url = config.get("API_URL")
+    api_token = config.get("API_TOKEN")
+    api_from_currency = config.get("API_FROM_CURRENCY")
+    api_to_currency = config.get("API_TO_CURRENCY")
+    influx_url = config.get("INFLUX_URL")
+    influx_token = config.get("INFLUX_TOKEN")
+    influx_org = config.get("INFLUX_ORG")
+    update_interval_sec = config.get("UPDATE_INTERVAL_SEC", default=30)
+
+    currency_repo = CurrencyRepository(api_token=api_token, base_url=api_url)
+    influx_writer = InfluxDBWriter(
+        influxdb_url=influx_url, influxdb_token=influx_token, influxdb_org=influx_org
+    )
+    currency_service = CurrencyService(currency_repo, influx_writer)
+
     while True:
-        data = get_data()
-        influx_data = as_influxdb(data)
-        await write_data(influx_data)
-        await asyncio.sleep(30)
+        await currency_service.fetch_and_store_currency_rate(
+            api_from_currency, api_to_currency
+        )
+        await asyncio.sleep(update_interval_sec)
 
 
 if __name__ == "__main__":
