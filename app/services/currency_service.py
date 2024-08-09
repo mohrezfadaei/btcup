d2126bca513fee016f3890966b5f3abc17e134da
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-09 23:37:33 +0330

    add services module

---
+++ app/services/currency_service.py
@@ -0,0 +1,12 @@
+class CurrencyService:
+    def __init__(self, currency_repo, influx_writer):
+        self.currency_repo = currency_repo
+        self.influx_writer = influx_writer
+
+    async def fetch_and_store_currency_rate(self, from_currency: str, to_currency: str):
+        try:
+            rate = self.currency_repo.get_exchange_rate(from_currency, to_currency)
+            formatted_data = self.influx_writer.format_data(formatted_data)
+            await self.influx_writer.write_data(formatted_data)
+        except Exception as e:
+            print(f"Error in fetch_and_store_currency_rate: {e}")
