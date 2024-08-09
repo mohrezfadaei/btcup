1907982f87f60747bf5cc99e4274be0e97e7995a
Mohammad Reza Fadaei <mohrezfadaei@gmail.com>
2024-08-10 00:51:32 +0330

    bug fix

--- app/services/currency_service.py
+++ app/services/currency_service.py
@@ -6,7 +6,7 @@ class CurrencyService:
     async def fetch_and_store_currency_rate(self, from_currency: str, to_currency: str):
         try:
             rate = self.currency_repo.get_exchange_rate(from_currency, to_currency)
-            formatted_data = self.influx_writer.format_data(formatted_data)
+            formatted_data = self.influx_writer.format_data(rate)
             await self.influx_writer.write_data(formatted_data)
         except Exception as e:
             print(f"Error in fetch_and_store_currency_rate: {e}")
